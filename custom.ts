c3c290915e211d9eb932cc45603397ef4d6918b3
Brammie15 <brammieverhulst@gmail.com>
2025-04-08 13:55:20 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -6,26 +6,22 @@ namespace Codecosmos {
    
     //% block="is %block=block under agent"
     //% block.shadow=minecraftBlock
-    //% block.loc.nl="is blok %block=block onder agent"
     export function test_for_block_under_agent(block : number) : boolean {
         return blocks.testForBlock(block, agent.getPosition().add(world(0,-1,0)))
     }
 
     //% block="water next to agent"
-    //% block.loc.nl="water naast agent"
     export function next_to_water() : boolean{
         return blocks.testForBlock(WATER, agent.getPosition().add(pos(0, -1, 1))) || blocks.testForBlock(WATER, agent.getPosition().add(pos(0, -1, -1)))
     }
 
     //% block="water next to agent"
-    //% block.loc.nl="water naast agent"
     export function next_to_water_extra(): boolean {
         return blocks.testForBlock(WATER, agent.getPosition().add(pos(1, -1, 0))) || blocks.testForBlock(WATER, agent.getPosition().add(pos(-1, -1, 0)))
     }
 
 
     //% block="close to fire"
-    //% block.loc.nl="dichtbij vuur"
     export function is_close_to_fire() : boolean{
         let isFire: boolean = false
         for(let i : number = -2; i <= 2; i++){
@@ -40,7 +36,6 @@ namespace Codecosmos {
 
 
     //% block="close to fire"
-    //% block.loc.nl="dichtbij vuur"
     export function is_close_to_fire_extra(): boolean {
         let isFire: boolean = false
         for (let i: number = -2; i < 3; i++) {
@@ -54,14 +49,12 @@ namespace Codecosmos {
     }
 
     //% block="plant tree"
-    //% block.loc.nl="plant boom"
     export function place_tree() : void{
         agent.setSlot(0);
         agent.place(DOWN);
     }
 
     //% block="setup"
-    //% block.loc.nl="Zet klaar"
     export function setup_1(): void{
         ///fill 225 74 43 244 74 43 air
         agent.setItem(JUNGLE_SAPLING, 64, 1)
@@ -71,7 +64,6 @@ namespace Codecosmos {
     }
 
     //% block="setup"
-    //% block.loc.nl="Zet klaar"
     export function setup_2(): void {
         ///fill 244 74 36 225 74 36 AIR
         agent.setItem(JUNGLE_SAPLING, 64, 1)
@@ -80,7 +72,6 @@ namespace Codecosmos {
     }
 
     //% block="setup"
-    //% block.loc.nl="Zet klaar"
     export function setup_3(): void {
         //fill 239 74 29 244 74 29 air
         agent.setItem(JUNGLE_SAPLING, 64, 1)
@@ -92,7 +83,6 @@ namespace Codecosmos {
     }
 
     //% block="setup"
-    //% block.loc.nl="Zet klaar"
     export function setup_extra1(): void {
         agent.setItem(JUNGLE_SAPLING, 64, 1)
         blocks.fill(AIR, world(224, 71, 5), world(224, 71, 11), FillOperation.Replace)
@@ -104,7 +94,6 @@ namespace Codecosmos {
     }
 
     //% block="setup"
-    //% block.loc.nl="Zet klaar"
     export function setup_extra2(): void {
         agent.setItem(JUNGLE_SAPLING, 64, 1)
         blocks.fill(AIR, world(234, 71, 11), world(234, 71, -2), FillOperation.Replace)
